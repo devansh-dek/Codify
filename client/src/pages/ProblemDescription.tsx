e27d6d7818c749b3f1c7cf1a5bbd99bb0a00bef5
Devansh <devanshksg@gmail.com>
2024-08-03 13:12:00 +0530

    Create Problem Description page and router

--- client/src/pages/ProblemDescription.tsx
+++ client/src/pages/ProblemDescription.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function ProblemDescription(Data: { title: string, description: string, sampleInput: string, sampleOutput: string, explanation: string }) {
+function ProblemDescription(Data: { title: string, description: string, sampleI  nput: string, sampleOutput: string, explanation: string }) {
     return (
         <div></div>
     )

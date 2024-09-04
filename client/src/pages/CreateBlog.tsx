726211e3721542cfdf59affd299877e7e6297cc2
Devansh <devanshksg@gmail.com>
2024-09-04 17:03:51 +0530

    Feature : Added create Blog Page

--- client/src/pages/CreateBlog.tsx
+++ client/src/pages/CreateBlog.tsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import userState from '../recoil/atoms/userState';
 
 function CreateBlog() {
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const navigate = useNavigate();
+    const user = useRecoilValue(userState);
+
+    const handleOnSubmit = async (e: any) => {
+        e.preventDefault();
+        try {
+            const formValue = {
+                title, description, user: user.username
+            };
+            console.log("form value is ", formValue);
+            const response = await axios.post('http://localhost:3000/api/v1/blogs', formValue);
+            console.log(response, "Is our response");
+
+            // if (response.data.exist === false) {
+            //     console.log("User doesn't exist");
+            //     return;
+            // }
+
+            // if (response.data.success === true) {
+            //     console.log("Came HERE");
+            //     const userLogged = {
+            //         userId: response.data.response.id,
+            //         username: response.data.response.username,
+            //         email: response.data.response.email,
+            //         isAuthenticated: true
+            // }
+            navigate('/blogs');
+
+            // Handle redirection or other actions on successful login
+            // }
+
+            console.log(response.data);
+        } catch (error: any) {
+            console.error('Login error:', error.response ? error.response.data : error.message);
+        }
+
+    }
+
     return (
-        <div>CreateBlog</div>
+        <div className='min-h-screen bg-slate-100 flex items-center justify-center'>
+            <div className='bg-white p-2 w-full max-w-lg h-full max-h-lg '>
+                <h1 className='font-bold text-4xl text-center'>Create a new blog</h1>
+                <form onSubmit={handleOnSubmit}>
+                    <div>
+                        <label className='font-semibold block text-2xl text-center m-2' htmlFor="title">Title</label>
+                        <input className='w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue m-2' type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Enter Blog Title' />
+
+                    </div>
+                    <div>
+                        <label className='font-semibold block text-2xl text-center m-2' htmlFor="Description">Description</label>
+                        <input className='w-full p-3 border h-32 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue m-2' type="text" id="description" value={description} onChange={(e) => setDescription(e.target.value)} placeholder='Enter Blog description' />
+
+                    </div>
+                    <button type="submit" className='border border-black w-full  rounded-md m-2'>Create Blog</button>
+                </form>
+
+            </div>
+
+
+
+        </div>
     )
 }
 
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRecoilValue } from 'recoil';
-import userState from '../recoil/atoms/userState'; // Adjust path as needed
+import userState from '../recoil/atoms/userState';
 import { Button } from '../components/ui/button.tsx';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 interface User {
     userId: any;
@@ -12,6 +13,10 @@ interface User {
 }
 function Profile() {
     const user = useRecoilValue(userState);
+    useEffect(async () => {
+        const fetchSubmissions = await axios.get(`http://localhost:3000/api/v1/submissions`);
+
+    }, [])
     const navigate = useNavigate();
     if (!user.isAuthenticated) {
         return <div className='font-bold w-full h-full justify-center items-center'>
@@ -21,6 +26,7 @@ function Profile() {
     const handleOnClick = (e: any) => {
         try {
             e.preventDefault();
+
             navigate('/createblog');
         } catch (error) {
             console.log(error);
@@ -29,11 +35,17 @@ function Profile() {
     }
     return (
         <div>
-            <div className='bg-slate-500 min-w-100 max-w-120 min-h-90 max-h-100 m-3 p-4'>
-                <h1 className='font-bold text-4xl' > USERNAME :  {user.username}</h1>
-                <h1 className='font-bold text-2xl'>EMAIL : {user.email}</h1>
-                {/* <Button variant="destructive">Destructive</Button> */}
-                <button className='border border-black' >CREATE A BLOG</button>
+            <div className=' min-w-100 max-w-120 min-h-90 border  border-slate-950  max-h-100 m-3 p-4'>
+                <h1 className='font-bold m-2 text-4xl' > USERNAME :  {user.username}</h1>
+                <h1 className='font-bold m-2 text-2xl'>EMAIL : {user.email}</h1>
+                <h1 className='font-bold m-2 text-2xl'>Question solved: 0</h1>
+                <button className='border m-2 p-2 border-black' onClick={handleOnClick}>CREATE A BLOG</button>
+            </div>
+            <div className='min-w-100 border border-slate-900'>
+                <h1 className='p-2 m-2 text-3xl font-bold '>Submissions</h1>
+                {/* //submissions all */}
+
+
             </div>
 
         </div>
